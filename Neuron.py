1047d188eb3dc39699bcd8793139f4fd5a866007
Brad Borget <bradley.borget@icloud.com>
2018-09-20 09:24:26 -0600

    Changed create_network function
    Create_network function now creates layers of neurons rather than just 1 neuron per layer.

--- Neuron.py
+++ Neuron.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 
 class Neuron:
-    def __init__(self, num_weights, inputs):
+    def __init__(self, num_weights):
         self.weights = []
         for i in range(num_weights + 1):
             self.weights.append(random.uniform(-1, 1))
-        self.input = inputs
+        #self.inputs = inputs
         #self.Bj = 0 #I can't remember what B sub j represents, so I'm commenting it out for now.  Putting in Error instead to represent the value that the neuron came to
         self.output = 0
         self.Error = 0
@@ -29,40 +29,40 @@ class NeuralNetModel:
         pass
 
 
+def create_network(layers, output_layer, num_weight):
+    layer_list = []
+    j = 0
+    layer_list.append(create_layer(num_weight, layers[j]))
+    for i in layers[-0]:
+        layer_list.append(create_layer(layers[j], i))
+        j += 1
+    layer_list.append(create_layer(layers[j], output_layer))
+    return layer_list
+
+
+def create_layer(num_inputs, numNeurons):
+    list = []
+    for i in range(0, numNeurons):
+        list.append(Neuron(num_inputs))
+    return list
+
+
+def back_propagate(layers, output_layer, target_value):
+    pass
+
+
+
 def main():
     inputs = pd.read_csv("C:\\Users\\bradl\\Documents\\Github\\Senior_Project-Neural_Net\\letter-recognition.txt")
     print(inputs)
-    print(len(set(inputs.letter)))
-
-    #results = []
-    #with open('example.csv') as File:
-    #    reader = csv.DictReader(File)
-    #    for row in reader:
-    #        results.append(row)
-
-    results = []
-    with open('letter-recognition.txt') as File:
-        reader = pd.read_csv(File)
-        for row in reader:
-            results.append(row)
-    num_weights = len(set(reader.letter))
+    output_layer = len(set(inputs.letter))
     num_layers = input("Enter number of hidden layers: ")
-    output_layer = 8
     layers = []
     for i in range(0, num_layers):
         layer = input("Enter number of Neurons for hidden layer " + str(i+1) + ": ")
         layers.append(layer)
-    neural_network = create_network(layers, output_layer, num_weights, results)
+    neural_network = create_network(layers, output_layer, (len(inputs.columns) - 1))
     print(len(neural_network))
-
-
-def create_network(layers, hidden_layer, num_weight, inputs):
-    neuron_list = []
-    for i in layers:
-        neuron = Neuron(num_weight, inputs)
-        neuron_list.append(neuron)
-    neuron_list.append(hidden_layer)
-    return neuron_list
 
 
 if __name__ == "__main__":
