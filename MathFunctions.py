172f92a90815d829074d558c34d418e281118896
10donovang <10donovang@gmail.com>
2018-10-03 09:41:48 -0600

    Forward Propagation

--- MathFunctions.py
+++ MathFunctions.py
@@ -1,7 +1,9 @@
 import math
 
+
 def oldOutputWeightUpdate(originalWeight, learningRate, actualOutput, expectedOutput, valuePassed): #outdated See new one below
     return originalWeight - (learningRate * (actualOutput - expectedOutput) * valuePassed) #w(ij) = w(ij) - n * (y(j) - t(j)) * x(i)
+
 
 def NeuronPassFail(listOfWeights, listOfInputs):
     i = 0
@@ -9,11 +11,14 @@ def NeuronPassFail(listOfWeights, listOfInputs):
         i += listOfInputs[index] * listOfWeights[index] #This should be weights * inputs
     return 1 / (1 + math.exp(-i))
 
+
 def weightUpdate(originalWeight, learningRate, errorOutput, input): #Requires Error function first
     return originalWeight - (learningRate * errorOutput * input)
 
+
 def getOutputError(actualOutput, targetOutput):
     return (actualOutput - targetOutput) * actualOutput * (1 - actualOutput)
+
 
 def getHiddenError(neuronPassFailValue, listOfWeights, error):
     i = 0
--- Neuron.py
+++ Neuron.py
@@ -1,11 +1,12 @@
 import random
 import pandas as pd
+import numpy as np
 
-#import MathFunctions as mf
+import MathFunctions as mf
 
 
 class Neuron:
-    def __init__(self, num_weights):
+    def __init__(self, num_weights, letter):
         self.weights = []
         for i in range(num_weights + 1):
             self.weights.append(random.uniform(-1, 1))
@@ -13,6 +14,7 @@ class Neuron:
         #self.Bj = 0 #I can't remember what B sub j represents, so I'm commenting it out for now.  Putting in Error instead to represent the value that the neuron came to
         self.output = 0
         self.Error = 0
+        self.letter = letter
 
 
 class NeuralNetClass:
@@ -42,11 +44,26 @@ def create_network(layers, output_layer, num_weight):
     return layer_list
 
 
-def create_layer(num_inputs, numNeurons):
-    list = []
-    for i in range(0, numNeurons):
-        list.append(Neuron(num_inputs))
-    return list
+def create_layer(num_inputs, num_neurons):
+    neuron_list = []
+    for i in range(0, num_neurons):
+        neuron_list.append(Neuron(num_inputs, ""))
+    return neuron_list
+
+
+def transfer(activate):
+    return 1.0 / (1.0 + np.exp(-activate))
+
+
+def forward_propagate(network, inputs):
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activate = mf.NeuronPassFail(neuron.weights, inputs)
+            neuron.output = transfer(activate)
+            new_inputs.append(neuron.ouput)
+        outputs = new_inputs
+    return outputs
 
 
 def back_propagate(layers, output_layer, target_value):
@@ -58,21 +75,21 @@ def back_propagate(layers, output_layer, target_value):
     #        neuron.
 
 
-
 def main():
-    inputs = pd.read_csv("C:\\Users\\bradl\\Documents\\Github\\Senior_Project-Neural_Net\\letter-recognition.txt")
+    inputs = pd.read_csv("letter-recognition.txt")
     output_layer = len(set(inputs.letter))
     letters = inputs.letter
-    newinputs = inputs.loc[:, inputs.columns != 'letter']
-    print(newinputs)
+    new_inputs = inputs.loc[:, inputs.columns != 'letter']
     num_layers = input("Enter number of hidden layers: ")
     layers = []
     for i in range(0, num_layers):
         layer = input("Enter number of Neurons for hidden layer " + str(i+1) + ": ")
         layers.append(layer)
-    neural_network = create_network(layers, output_layer, len(newinputs.columns))
-    print(len(neural_network))
+    neural_network = create_network(layers, output_layer, len(new_inputs.columns))
+    outputs = forward_propagate(neural_network, new_inputs)
 
 
 if __name__ == "__main__":
     main()
+
+# cycle through each Neuron in the output layer and assign a letter to all 26 Neurons
